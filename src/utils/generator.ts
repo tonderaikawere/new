469974e7a6cf8b73d48391f8e3605944c007db2b
tonderaikawere <tondeskawere@gmail.com>
2026-07-19 18:41:11 +0200

    utils: create generator helper setup with pool definitions

---
+++ src/utils/generator.ts
@@ -0,0 +1,6 @@
+import { GenOptions } from '../types';
+
+export const lowercaseSet = 'abcdefghijklmnopqrstuvwxyz';
+export const uppercaseSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const numberSet = '0123456789';
+export const symbolSet = '!@#$%^&*()_+-=[]{}|;:\',./<>?';
